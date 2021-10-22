2a11a40e02fc96701f23f27a55a788a262ae77af
Kishan Bagga <kishansinghbagga@gmail.com>
2021-10-22 23:50:18 +0530

    some extra knowledge

--- Strings/code/src/com/kishan/Palin.java
+++ Strings/code/src/com/kishan/Palin.java
@@ -4,10 +4,17 @@ public class Palin {
     public static void main(String[] args) {
         String str = "abccba";
         String str1 = null;
-        String str2 = "";
-        System.out.println(isPalindrome(str));
+        String                       str2 = "";
+        String str3; //unused variable
+//        String str3="";
+        System.out.println("isPalindrome: "+isPalindrome(str));
+        // all the comments,tabs are removed while generating .class file
+        /* by the Lexical Analyzer
+        present in Compiler
+         */
     }
 
+    // fun for checking if a string is palindrome or not
     static boolean isPalindrome(String str) {
         if (str == null || str.length() == 0) {
             return true;
